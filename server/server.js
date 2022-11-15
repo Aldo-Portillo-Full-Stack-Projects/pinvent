f5cfb584ccf251d9668f5d7343a5f5dded239cb7
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-15 14:20:43 -0600

    Connect to DB and start server

--- server/server.js
+++ server/server.js
@@ -4,3 +4,16 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors")
 
+const app = express()
+
+const PORT = process.env.PORT || 5000;
+
+//Connect to DB and Start Server
+mongoose 
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server Running on port ${PORT}`);
+        })
+    })
+    .catch((err) => console.log(err);)
