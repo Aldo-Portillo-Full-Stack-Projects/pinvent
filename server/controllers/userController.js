932840697900b42ae5bac39956c52afdb55ca1af
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-15 23:15:10 -0600

    Hash password

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,7 @@
 const asyncHandler = require("express-async-handler")
 const User = require("../models/userModel")
+const bcrypt = require('bcryptjs')
+
 
 
 const registerUser = asyncHandler( async (req, res) => {
@@ -23,11 +25,15 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new Error("Email is already registered")
     }
 
+    //Encrypt Password before sending to DB
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt) //Password is destructured otherwise it'll be req.body.password
+
     //Create User
     const user = await User.create({
         name,
         email,
-        password,
+        password: hashedPassword,
     })
 
     if(user) {
