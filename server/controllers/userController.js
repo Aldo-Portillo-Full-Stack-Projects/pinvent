e8ae55c01bb66a5ad26f7cecc1e43bad59bc1518
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-17 13:02:15 -0600

    Add login controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,12 +1,14 @@
 const asyncHandler = require("express-async-handler")
 const User = require("../models/userModel")
 const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
 
 //Create JSON web token using user id and a secret string to encrypt and add expiration
 const generateToken = (id) => {
     return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: "1d"})
 }
 
+//Register user
 const registerUser = asyncHandler( async (req, res) => {
     const {name, email, password} = req.body
 
@@ -58,6 +60,53 @@ const registerUser = asyncHandler( async (req, res) => {
     }
 })
 
+//Login user
+const loginUser = asyncHandler( async (req, res) => {
+    const {email, password } = req.body;
+
+    //Validate request
+    if(!email || !password) {
+        res.status(400)
+        throw new Error("Please add email and password")
+    }
+
+    //Check if user exists
+    const user = await User.findOne({email})
+
+    if(!user){
+        res.status(400)
+        throw new Error("User not found, please sign up")
+    }
+    
+    //Check if password is correct
+    const passwordIsCorrect = await bcrypt.compare(password, user.password)
+
+    //Generate Token
+    const token = generateToken(user._id)
+
+    //Send HTTP-only cookie to front end
+    res.cookie("token", token, {
+        path: "/",
+        httpOnly: true,
+        expires: new Date(Date.now() + 1000 * 86400),
+        sameSite: "none", //Only used in deployment allows it to work on deployment
+        secure: true, //Only used in deployment
+    })
+
+    //Get user info if validated
+    if (user && passwordIsCorrect) {
+        const { _id, name, email, photo, phone, bio }  = user
+        res.status(200).json({
+            _id, name, email, photo, phone, bio, token
+        })
+    } else {
+        res.status(400)
+        throw new Error("Invalid Email or Password")
+    }
+})
+ 
 module.exports = {
     registerUser,
+    loginUser,
+
 }
