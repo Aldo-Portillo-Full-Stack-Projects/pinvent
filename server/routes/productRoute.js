00ae74368e60e2f5ed2db090dbb7daa39008e72d
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-18 09:24:13 -0600

    Add delete product router with params

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createProduct, getProducts, getProduct } = require("../controllers/productController");
+const { createProduct, getProducts, getProduct, deleteProduct } = require("../controllers/productController");
 const router = express.Router();
 const protect = require("../middleware/authMiddleware");
 const {upload} = require('../utils/fileUpload')
@@ -7,5 +7,6 @@ const {upload} = require('../utils/fileUpload')
 router.post("/", protect, upload.single("image"), createProduct) //Change single to array to add multiple
 router.get("/", protect, getProducts)
 router.get("/:id", protect, getProduct)
+router.delete("/:id", protect, deleteProduct)
 
 module.exports = router
