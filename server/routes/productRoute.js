db4d62dec080228fe186dcfc335e82ba3bd6314a
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-18 09:11:21 -0600

    Create get products route

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { createProduct } = require("../controllers/productController");
+const { createProduct, getProducts } = require("../controllers/productController");
 const router = express.Router();
 const protect = require("../middleware/authMiddleware");
 const {upload} = require('../utils/fileUpload')
 
 router.post("/", protect, upload.single("image"), createProduct) //Change single to array to add multiple
+router.get("/", protect, getProducts)
 
 module.exports = router
