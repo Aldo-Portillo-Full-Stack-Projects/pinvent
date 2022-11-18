53134e24827600180fb4dd7223edbe348be869d6
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-18 07:54:07 -0600

    Write createProduct Controller function

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,7 +1,30 @@
 const asyncHandler = require("express-async-handler")
+const Product = require("../models/productModel")
 
 const createProduct = asyncHandler(async (req, res) => {
-    res.send("Create Product")
+    const {name, sku, category, quantity, price, description} = req.body
+
+    //validation
+    if (!name || !category || !quantity || !price || !description){
+        res.status(400)
+        throw new Error("Please fill in all fields")
+    }
+
+    //Image upload
+
+    //Create Product
+    const product = await Product.create({
+        userId: req.user.id, //Might change to user in model
+        name,
+        sku,
+        category,
+        quantity,
+        price,
+        description,
+        //image,
+    })
+
+    res.status(201).json(product)
 })
 
 module.exports = {
