08d078b87e68e48a435149a96b494979398589dc
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-18 09:53:51 -0600

    Fix routes to modify the product

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -5,9 +5,9 @@ const protect = require("../middleware/authMiddleware");
 const {upload} = require('../utils/fileUpload')
 
 router.post("/", protect, upload.single("image"), createProduct) //Change single to array to add multiple
+router.patch("/:id", protect, upload.single("image"), updateProduct);
 router.get("/", protect, getProducts)
 router.get("/:id", protect, getProduct)
 router.delete("/:id", protect, deleteProduct)
-router.patch("/:id", protect, updateProduct)
 
 module.exports = router
