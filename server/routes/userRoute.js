c7563ec47604dbeb7babebabceea5b8e97305956
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-18 06:55:03 -0600

    Add resetpassword put route with params

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { registerUser, loginUser, logout, getUser, loginStatus, updateUser, changePassword, forgotPassword} = require("../controllers/userController");
+const { registerUser, loginUser, logout, getUser, loginStatus, updateUser, changePassword, forgotPassword, resetPassword} = require("../controllers/userController");
 const protect = require("../middleware/authMiddleware");
 const router = express.Router();
 
@@ -14,6 +14,7 @@ router.patch("/updateuser", protect, updateUser)
 router.patch("/changepassword", protect, changePassword)
 
 router.post("/forgotpassword",  forgotPassword)
+router.put("/resetpassword/:resetToken", resetPassword)
 
 
 
