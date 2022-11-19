7c39a2f110ae3cf89e82d86ace6574c2cbffe487
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-19 13:50:27 -0600

    Add routes for auth pages

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,19 @@ import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
+import Reset from './pages/auth/Reset';
+import Forgot from './pages/auth/Forgot';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/resetpassword/:resetToken" element={<Reset />} />
+        <Route path="/forgot" element={<Forgot />} />
       </Routes>
     </BrowserRouter>
   );
