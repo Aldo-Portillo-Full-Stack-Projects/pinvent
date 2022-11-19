287cf48f3658d16093a8ca7e21b645a322f40a49
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-19 13:44:55 -0600

    Update imports

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
 
 function App() {
   return (
