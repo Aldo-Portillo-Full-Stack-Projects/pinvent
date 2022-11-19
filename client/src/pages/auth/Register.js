cf8ec84228e85c1eda16dd206d0d0b41e36c0c5e
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-19 14:38:39 -0600

    Create register page

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import styles from "./auth.module.scss"
+import { AiOutlineUserAdd } from "react-icons/ai"
+import Card from "../../components/card/Card"
+import {Link } from 'react-router-dom'
 
 export default function Register() {
   return (
-    <div>
-        <h1> Register Page
-        </h1></div>
+    <div className={`container ${styles.auth}`}>
+      <Card>
+        <div className={styles.form}>
+          <div className='--flex-center'>
+            <AiOutlineUserAdd size={35} color="#999" />
+          </div>
+          <h2>Register</h2>
+
+          <form>
+            <input type="text" placeholder='Name' required name="name" />
+            <input type="text" placeholder='Email' required name="email" />
+            <input type="password" placeholder='Password' required name="password" />
+            <input type="password" placeholder='Confirm Password' required name="confirmPassword" />
+            <button type="submit" className='--btn --btn-primary --btn-block'>Register</button>
+          </form>
+
+          <span className={styles.register}>
+            <Link to="/">Home</Link>
+            <p>&nbsp; Already have an account? &nbsp;</p>
+            <Link to="/login">Login</Link> 
+          </span>
+        </div>
+      </Card>
+    </div>
   )
 }
