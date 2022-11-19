409d0de6327089a1d92df01c0a3f9a0dfd372326
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-19 13:43:58 -0600

    Remove authentication pages from pages

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {RiProductHuntLine} from "react-icons/ri"
 import { Link } from 'react-router-dom'
 import "./Home.scss"
+import heroImg from "../assets/inv-img.png"
 
 export default function Home() {
   return (
@@ -26,6 +27,35 @@ export default function Home() {
                 </li>
             </ul>
         </nav>
+        {/*HEEO*/}
+        <section className='container hero'>
+            <div className='hero-text'>
+                <h2>Inventory { "&" } Stock Management Solution</h2>
+                <p>Inventory system to control and manage products in the warehouse in real time and integrated to make it easier to develop your business.</p>
+                <div className='hero-buttons'>
+                <button className='--btn --btn-secondary'>
+                        <Link to="/register">1 Month Free Trial</Link>
+                    </button>
+                </div>
+                <div className='--flex-start'>
+                    <NumberText num="14K" text="Brand Owners" />
+                    <NumberText num="23K" text="Active Users" />
+                    <NumberText num="500+" text="Partners" />
+                </div>
+            </div>
+            <div className='hero-image'>
+                <img src={heroImg} alt="Inventory" />
+            </div>
+        </section>
     </div>
   )
+}
+
+const NumberText = ({num, text}) => {
+    return (
+        <div className='--mr'>
+            <h3 className="--color-white">{num}</h3>
+            <p className="--color-white">{text}</p>
+        </div>
+    )
 }
