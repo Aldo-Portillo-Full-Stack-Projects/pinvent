89897af839cf5d69df4bf450e30767871d29d4a0
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-20 15:36:17 -0600

    Code header component

---
+++ client/src/components/header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='--pad header'>
+        <div className='--flex-between'>
+            <h3>
+                <span className='--fw-thin'>Welcome, </span>
+                <span className='--color-danger'>Aldo</span>
+            </h3>
+            <button className='--btn --btn-danger'>Logout</button>
+        </div>
+        <hr />
+    </div>
+  )
+}
