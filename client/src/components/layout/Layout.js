36662ed6fabc8d6fe2de972e071b303547f938dd
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-20 15:36:52 -0600

    Code layout component with children props

---
+++ client/src/components/layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer from '../footer/Footer'
+import Header from '../header/Header'
+
+export default function Layout({children}) {
+  return (
+    <>
+        <Header />
+        <div style={{minHeight: "8-vh"}} className="--pad">{children}</div>
+        <Footer />
+    </>
+  )
+}
