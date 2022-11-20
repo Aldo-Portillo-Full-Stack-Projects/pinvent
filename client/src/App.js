503e04e5f3e5c7a81aa9137926a52b6512841388
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-20 16:13:55 -0600

    Render dashboard with nested components passing down children props

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,31 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import Reset from './pages/auth/Reset';
 import Forgot from './pages/auth/Forgot';
+import Sidebar from './components/sidebar/Sidebar';
+import Layout from './components/layout/Layout';
+import Dashboard from './pages/dashboard/Dashboard';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/resetpassword/:resetToken" element={<Reset />} />
         <Route path="/forgot" element={<Forgot />} />
+
+        <Route path="/dashboard" element={
+          <Sidebar>
+            <Layout>
+              <Dashboard />
+            </Layout>
+          </Sidebar>
+        } />
       </Routes>
     </BrowserRouter>
   );
