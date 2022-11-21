e3b7c304251f114921428fec25934fbdc0b6d185
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-21 11:08:49 -0600

    Reuse code from register to log user sumitted data to console

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -2,9 +2,34 @@ import React from 'react'
 import styles from "./auth.module.scss"
 import { BiLogIn} from "react-icons/bi"
 import Card from "../../components/card/Card"
-import {Link } from 'react-router-dom'
+import {Link, useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+
+
+const initialState = {
+  email: "",
+  password: "",
+}
+
 
 export default function Login() {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const [isLoading, setIsLoading] = React.useState(false)
+  const [formData, setFormData] = React.useState(initialState)
+  const { email, password } = formData
+
+  const handleInputChange = (e) => {
+    const {name, value} = e.target;
+    setFormData({...formData, [name]: value})
+  }
+
+  const login = async (e) => {
+    e.preventDefault();
+
+    console.log(formData)
+  } 
+
   return (
     <div className={`container ${styles.auth}`}>
       <Card>
@@ -14,9 +39,9 @@ export default function Login() {
           </div>
           <h2>Login</h2>
 
-          <form>
-            <input type="text" placeholder='Email' required name="email" />
-            <input type="password" placeholder='Password' required name="password" />
+          <form onSubmit={login}>
+            <input type="text" placeholder='Email' required name="email" value={email} onChange={handleInputChange}/>
+            <input type="password" placeholder='Password' required name="password" value={password} onChange={handleInputChange}/>
             <button type="submit" className='--btn --btn-primary --btn-block'>Login</button>
           </form>
           <Link to="/forgot">Forgot Password</Link>
