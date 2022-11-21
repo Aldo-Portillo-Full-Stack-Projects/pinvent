5d378a3aab6cdf4b2e35b307016a6ee31bf5faf2
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-20 18:37:23 -0600

    Link P logo to homepage using useNavigate react-router hook

--- client/src/components/sidebar/Sidebar.js
+++ client/src/components/sidebar/Sidebar.js
@@ -4,6 +4,7 @@ import {HiMenuAlt3} from 'react-icons/hi'
 import menu from "../../data/sidebar"
 import SidebarItem from "./SidebarItem"
 import React from "react"
+import { useNavigate } from "react-router-dom"
 
 export default function Sidebar({children}) {
 
@@ -15,12 +16,18 @@ export default function Sidebar({children}) {
     return <SidebarItem key={index} item={item} isOpen={isOpen} />
   })
 
+  const navigate = useNavigate()
+
+  const goHome = () => {
+    navigate("/")
+  }
+
   return (
     <div className='layout'>
         <div className='sidebar' style={{ width: isOpen ? "230px": "60px"}}>
             <div className="top_section">
               <div className="logo" style={{display: isOpen ? "block": "none"}}>
-                <RiProductHuntLine size={35} style={{cursor: "pointer"}}/>
+                <RiProductHuntLine size={35} style={{cursor: "pointer"}} onClick={goHome}/>
               </div>
               <div className="bars" style={{marginLeft: isOpen ? "135px" : "0px"}}>
                 <HiMenuAlt3 size={35} onClick = {toggle} style={{cursor: "pointer", }}/>
