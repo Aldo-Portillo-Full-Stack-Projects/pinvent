b088bb0624530bd3c741876b56895adbc98c8f1d
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-21 12:04:40 -0600

    Set validation and pass emailData to auth services

--- client/src/pages/auth/Forgot.js
+++ client/src/pages/auth/Forgot.js
@@ -3,8 +3,30 @@ import styles from "./auth.module.scss"
 import { AiOutlineMail} from "react-icons/ai"
 import Card from "../../components/card/Card"
 import {Link } from 'react-router-dom'
+import { forgotPassword, validateEmail } from '../../services/authService'
+import {toast} from 'react-toastify'
 
 export default function Forgot() {
+  const [email, setEmail] = React.useState()
+
+  const forgot = async (e) => {
+    e.preventDefault();
+
+    if( !email ){
+      return toast.error("Please enter an email")
+    }
+
+    if(!validateEmail(email)){
+      return toast.error("Please enter a valid email")
+    }
+
+    const emailData = {
+      email,
+    }
+
+    await forgotPassword(emailData);
+    setEmail("")
+  }
   return (
     <div className={`container ${styles.auth}`}>
       <Card>
@@ -14,8 +36,8 @@ export default function Forgot() {
           </div>
           <h2>Forgot Password</h2>
 
-          <form>
-            <input type="text" placeholder='Email' required name="email" />
+          <form onSubmit={forgot}>
+            <input type="text" placeholder='Email' required name="email" value={email} onChange={(e)=> setEmail(e.target.value)}/>
             <button type="submit" className='--btn --btn-primary --btn-block'>Get Reset Email</button>
             <div className={styles.links}>
               <Link to="/">- Home -</Link>
