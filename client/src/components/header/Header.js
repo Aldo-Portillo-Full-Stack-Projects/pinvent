faf78e0e835e3f5e90e377ec3ef63a1634cab53d
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-21 11:52:51 -0600

    Add logout functionality to logout button in header

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,14 +1,29 @@
 import React from 'react'
+import { logoutUser } from '../../services/authService'
+import { useDispatch, useSelector } from 'react-redux';
+import { selectName, SET_LOGIN } from '../../redux/features/authSlice';
+import {useNavigate } from 'react-router-dom';
 
+// 🐛: If you travel to /dashboard after clicking logout the name still appears will fix later
 export default function Header() {
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const name = useSelector(selectName)
+
+  const logout = async () => {
+    await logoutUser();
+    await dispatch(SET_LOGIN(false))
+    navigate("/login")
+  }
   return (
     <div className='--pad header'>
         <div className='--flex-between'>
             <h3>
                 <span className='--fw-thin'>Welcome, </span>
-                <span className='--color-danger'>Aldo</span>
+                <span className='--color-danger'>{name}</span> 
             </h3>
-            <button className='--btn --btn-danger'>Logout</button>
+            <button className='--btn --btn-danger' onClick={logout}>Logout</button>
         </div>
         <hr />
     </div>
