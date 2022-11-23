6941c26ab16b8589b5bf4aa04ec4fc86a1e19f31
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-23 15:48:08 -0600

    Create cases for pending success and fufilled in deleteProduct

--- client/src/redux/features/productSlice.js
+++ client/src/redux/features/productSlice.js
@@ -147,6 +147,22 @@ const productSlice = createSlice({
             state.message = action.payload
             toast.error(action.payload)
           })
+          //Delete Product cases
+          .addCase(deleteProduct.pending, (state) => {
+            state.isLoading = true
+          })
+          .addCase(deleteProduct.fulfilled, (state, action) => {
+            state.isLoading = false
+            state.isSuccess = true
+            state.isError = false
+            toast.success("Product deleted")
+          })
+          .addCase(deleteProduct.rejected, (state, action) => {
+            state.isLoading = false
+            state.isError = true
+            state.message = action.payload
+            toast.error(action.payload)
+          })
           
   }
 });
