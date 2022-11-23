949a5899c8074225421e77959ce9974c7b87aacd
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-23 13:38:59 -0600

    Add the reducer to store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit"
 import authReducer from '../redux/features/authSlice'
 import productReducer from '../redux/features/productSlice'
+import filterReducer from '../redux/features/filterSlice'
 
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         product: productReducer,
+        filter: filterReducer,
     }
 })
