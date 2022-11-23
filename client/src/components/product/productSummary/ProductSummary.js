8efb9f22fff5faa4f6175cb87910694b45aa9ba8
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-23 15:13:58 -0600

    Fix format function and apply it to count prop

--- client/src/components/product/productSummary/ProductSummary.js
+++ client/src/components/product/productSummary/ProductSummary.js
@@ -13,8 +13,8 @@ const productIcon = <BsCart4 size={40} color="#fff" />
 const categoryItem = <BiCategory size={40} color="#fff" />
 const outOfStockIcon = <BsCartX size={40} color="#fff" />
 
-export const formatNumbers = (x) => {
-  return x.toString().replace(/\B(?=(\d{3}) + (?!\d))/g, ",")
+const formatNumbers = (x) => {
+  return x.toString().split( /(?=(?:\d{3})+(?:\.|$))/g ).join( "," );
 }
 export default function ProductSummary({products}) {
   const dispatch = useDispatch();
@@ -28,7 +28,7 @@ export default function ProductSummary({products}) {
       <h3 className='--mt'>Inventory Stats</h3>
       <div className='info-summary'>
         <InfoBox icon={productIcon} title={"Total Products"} count={products.length} bgColor="card1"/>
-        <InfoBox icon={earningIcon} title={"Total Store Value"} count={`$ ${totalStoreValue}`} bgColor="card2"/>
+        <InfoBox icon={earningIcon} title={"Total Store Value"} count={`$ ${formatNumbers(totalStoreValue.toFixed(2))}`} bgColor="card2"/>
         <InfoBox icon={outOfStockIcon} title={"Out of Stock"} count={0} bgColor="card3"/>
         <InfoBox icon={categoryItem} title={"All Categories"} count={1} bgColor="card4"/>
       </div>
