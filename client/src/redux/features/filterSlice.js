ce519cd913c8e3a9ce1bd55e4017fd0f3451ee46
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-23 13:36:38 -0600

    Write filter slice

---
+++ client/src/redux/features/filterSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    filteredProducts: [],
+}
+
+const filterSlice = createSlice({
+  name: "filter",
+  initialState,
+  reducers: {
+    FILTER_BY_SEARCH(state, action) {
+
+    }
+  }
+});
+
+export const {FILTER_BY_SEARCH} = filterSlice.actions
+
+export const selectFilteredProduct = state.filter.filteredProducts
+
+export default filterSlice.reducer
