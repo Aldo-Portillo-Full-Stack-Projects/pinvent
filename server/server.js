4e7b38a1631added1694880a4cdba0160a79011a
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-27 16:34:57 -0600

    Remove / after front end url

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(cookieParser())
 app.use(express.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use(cors({
-    origin: ["http://localhost:3000", "https://pinvent-portillo-app.vercel.app/"], //Make sure to add url to active site here
+    origin: ["http://localhost:3000", "https://pinvent-portillo-app.vercel.app"], //Make sure to add url to active site here
     credentials: true
 }))
 
